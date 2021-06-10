8ed6088e7a468ef24f26a1aededd8183c3cffc8f
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-10 14:47:13 -0400

    first push

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:YOURPASSWORD@localhost:5432/SERVERNAME');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
