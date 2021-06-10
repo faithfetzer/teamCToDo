370858eab56bf6e140c6e5ec5fc80000e0c1b507
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-10 16:36:14 -0400

    heroku setup

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const Express = require('express');
 const app = Express();
 app.use(Express.json());
 
-// const dbConnection = require('./db');
+const dbConnection = require('./db');
 // const middleware =require('./middleware');
 // const controllers = require('./controllers');
 // app.use(middleware.headers);
 
+app.use('/test', (req, res) => {
+    res.send('test message')
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`[Server]: App is listening on ${process.env.PORT}`);
