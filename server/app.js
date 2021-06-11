74b9478e804100cef16bfabe5f30872195828b3c
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-10 20:45:05 -0400

    little changes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,36 @@
-require('dotenv').config();
+// require('dotenv').config();
 const Express = require('express');
+const { DataTypes } = require('sequelize/types');
 const app = Express();
 app.use(Express.json());
-
+ app.use('/list', controllers.listController);
+ app.use('/user', controllers.userController)
 const dbConnection = require('./db');
 // const middleware =require('./middleware');
-// const controllers = require('./controllers');
+ const controllers = require('./controllers');
 // app.use(middleware.headers);
+
+
+const List = db.define('list', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.INTEGER,   
+        allowNull:false
+    },
+    timedue: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+
+    }
+
+
+})
 
 app.use('/test', (req, res) => {
     res.send('test message')
--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,17 @@
 // build user model
 // first name, last name, email, password- all required
 // in requirements of project said something about email formatting so need to make sure we hvae that correct
+const {DataTypes} = require('sequelize')
+const db = require('../db')
+
+const User = db.define('user', {
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        //unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
