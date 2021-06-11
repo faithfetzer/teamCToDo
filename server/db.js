53af20e57412e46ff6cbeb8de476ecc50ccaf93a
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-06-11 14:21:09 -0400

    list updated

--- server/db.js
+++ server/db.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const Sequelize = require('sequelize');
 
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
-const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
+ const sequelize = new Sequelize(process.env.DATABASE_URL);
+// const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
 
 
 module.exports = sequelize;
