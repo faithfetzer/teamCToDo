a3bf4e82a094972a134c19bc7425dd411105195d
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-12 19:53:05 -0400

    small changes, working on getting server functional

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: true
+//     }});
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
+const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
 
 
 module.exports = sequelize;
