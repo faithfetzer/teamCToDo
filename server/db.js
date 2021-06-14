2a8213a71b7bb014d5a372bb6b0caa9055d90f63
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-14 15:17:09 -0400

    new user endpoint

--- server/db.js
+++ server/db.js
@@ -1,19 +1,13 @@
 require('dotenv').config()
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
 
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
+})
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: true
-//     }});
-
-const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
-
-
-    //const sequelize = new Sequelize(process.env.DATABASE_URL_LOCAL);
 
 module.exports = sequelize;
